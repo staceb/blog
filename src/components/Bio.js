30e5f2dba873f4a1aed0188cc97074fde2787fc9
Matt Canty <matthewcanty@gmail.com>
2018-06-12 08:32:05 +0100

    Add leJog blog post

--- src/components/Bio.js
+++ src/components/Bio.js
@@ -27,10 +27,10 @@ class Bio extends React.Component {
           }}
         />
         <p>
-          Written by <strong>Kyle Mathews</strong> who lives and works in San
-          Francisco building useful things.{' '}
-          <a href="https://twitter.com/kylemathews">
-            You should follow him on Twitter
+           Written by <strong>Matt Canty</strong>. If you have any comments
+           or want to start a conversation,{' '}
+          <a href="mailto:matthewcanty@gmail.com">
+            email me.
           </a>
         </p>
       </div>
